392fd83b117c0de489079f3eaa3b6665b90c62e5
shrishti <shrishti@zeeve.io>
2022-03-29 01:48:00 +0530

    integration

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Page from './components/Page';
+import { Connection } from './metamask';
 
 function App() {
   const [isLogin, setLogin] = useState(false);
   const [isWalletLogin, setWalletLogin] = useState(false)
+  
+
+
   return (
     <div className="App bg-BackroundColor h-full relative">
-      <Header isLogin={isLogin} isWalletLogin={isWalletLogin} />
-      <Page isLogin={isLogin} />
+      <Header isLogin={isLogin} isWalletLogin={isWalletLogin} setLogin={setLogin} setWalletLogin={setWalletLogin}/>
+      <Page isLogin={isLogin} setLogin={setLogin}/>
     </div>
   );
 }
